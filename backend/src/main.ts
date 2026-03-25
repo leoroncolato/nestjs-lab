210531633e9e8e94dcea91b174432b955cc3a831
leoroncolato <leonardo.p.froncolato@gmail.com>
2026-03-25 20:35:51 -0300

    docs: configuracao do swagger openapi

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Cinema Lab API')
+    .setDescription('API para gerenciamento de perfis, usuários e endereços [cite: 4]')
+    .setVersion('1.0')
+    .addBearerAuth() // Para o Desafio Extra de JWT
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document); // Rota /api/docs 
+
+  await app.listen(3000);
 }
 bootstrap();

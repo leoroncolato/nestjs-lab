d2cf278af8ef256bac10868bb9d84c6338c3d18d
Leonardo Roncolato <146259738+leoroncolato@users.noreply.github.com>
2026-03-25 21:58:28 -0300

    EDIT SERVICE2

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,39 +2,36 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async create(data: CreateUserDto) {
-    const hashedPassword = await bcrypt.hash(data.password, 10);
-    return this.prisma.user.create({
-      data: { ...data, password: hashedPassword },
-    });
+  create(dto: CreateUserDto) {
+    return this.prisma.user.create({ data: dto });
   }
 
   findAll() {
     return this.prisma.user.findMany({
-      where: { deletedAt: null }, // Soft delete filter
-      include: { profile: true, address: true },
+      include: { profile: true, address: true }, // inclui relações
     });
   }
 
   findOne(id: string) {
-    return this.prisma.user.findUnique({ where: { id } });
+    return this.prisma.user.findUnique({
+      where: { id },
+      include: { profile: true, address: true },
+    });
   }
 
-  update(id: string, data: UpdateUserDto) {
-    return this.prisma.user.update({ where: { id }, data });
+  update(id: string, dto: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: { id },
+      data: dto,
+    });
   }
 
   remove(id: string) {
-    // Soft delete: atualiza o deletedAt ao invés de deletar a linha
-    return this.prisma.user.update({
-      where: { id },
-      data: { deletedAt: new Date() },
-    });
+    return this.prisma.user.delete({ where: { id } });
   }
 }
